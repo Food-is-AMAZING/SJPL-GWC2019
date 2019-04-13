59e79480bb64384ab01de72d1ee3fa3ca00d5009
Tazmeen Ahmed <waddlepenguin0409@gmail.com>
2019-04-12 17:04:53 -0700

    changed the extension of js and css files

---
+++ Javascript/global-warming.js
@@ -0,0 +1,20 @@
+﻿function funFact() {
+  
+var firstFact = prompt("Add Text")
+  
+  
+var answerThis = prompt("Was this information useful to you?")
+  
+var newT = document.getElementById("text");
+  
+if (answerThis = "yes") {
+    newT.innerHTML = "Thank you for learning!";
+  } else if (answerThis = "no") {
+    newT.innerHTML = "Thank you for reading!";
+  } else {
+    newT.innerHTML = "Thank you for learning!";
+  }
+  
+newT.style.fontSize ="11px";
+  newT.style.color = "black"; 
+  
