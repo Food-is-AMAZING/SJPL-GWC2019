65375eb4c02b616078545e64e0e2762f1fac5018
Tazmeen Ahmed <waddlepenguin0409@gmail.com>
2019-04-19 17:10:40 -0700

    Added content to .js and .css files

--- Javascript/global-warming.js
+++ Javascript/global-warming.js
@@ -1,20 +1,32 @@
 ﻿function funFact() {
   
-var firstFact = prompt("Add Text")
+  var firstFact = prompt("Add Text")
   
   
-var answerThis = prompt("Was this information useful to you?")
+
+  var answerThis = prompt("Was this information useful to you?")
   
-var newT = document.getElementById("text");
+  var newT = document.getElementById("text");
   
-if (answerThis = "yes") {
-    newT.innerHTML = "Thank you for learning!";
+  if (answerThis = "yes") {
+    
+     newT.innerHTML = "Thank you for learning!";
+  
   } else if (answerThis = "no") {
-    newT.innerHTML = "Thank you for reading!";
+    
+     newT.innerHTML = "Thank you for reading!";
+  
   } else {
-    newT.innerHTML = "Thank you for learning!";
+    
+     newT.innerHTML = "Thank you for learning!";
+  
   }
   
-newT.style.fontSize ="11px";
-  newT.style.color = "black"; 
+
+
+  newT.style.fontSize ="11px";
   
+     newT.style.color = "black"; 
+ 
+
+} 
